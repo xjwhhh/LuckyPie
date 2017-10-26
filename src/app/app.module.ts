779141cc330235aae42a412aad8c49f2372bb1cc
151250171 <151250171@smail.nju.edu.cn>
2017-10-26 17:00:21 +0800

    Overall architecture

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+
+import {Router} from '@angular/router';
+import {AppRoutingModule} from './app-routing.module';
+
+import {FollowModule} from './follow/follow.module';
+import {HotModule} from './hot/hot.module';
+import {ExploreModule} from './explore/explore.module';
+import {IdentifyModule} from './identify/identify.module';
+import {NoticeModule} from './notice/notice.module';
+import {PostModule} from './post/post.module';
+
+import {BsDropdownModule} from 'ngx-bootstrap';
+import {ImageUploadModule} from 'angular2-image-upload';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BsDropdownModule.forRoot(),
+    ImageUploadModule.forRoot(),
+    ExploreModule,
+    FollowModule,
+    HotModule,
+    IdentifyModule,
+    NoticeModule,
+    PostModule,
+    AppRoutingModule,
   ],
-  providers: [],
+  declarations: [AppComponent],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
